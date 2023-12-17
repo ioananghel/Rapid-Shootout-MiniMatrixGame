d5fec2dd912284ec65c631f909f739d2fdd64a72
Ioan Anghel <ioan.anghel123@gmail.com>
2023-12-17 18:54:58 +0200

    Added visual map of memory

--- mini_matrix_game/memory.h
+++ mini_matrix_game/memory.h
@@ -7,3 +7,20 @@
 
 #define highScoresAddress soundSettingsAddress + sizeof(int)
 #define numberOfHighScores 3
+
+// visual represantation of the memory:
+/*               addr (in EEPROM)
+---------------- 1024 (Max size)
+...
+---------------- 934
+Top 3 Players with High Scores
+---------------- 910 (highScoresAddress)
+Sound Settings (ON/OFF)
+---------------- 906 (soundSettingsAddress)
+Auto Matrix Brightness (ON/OFF)
+---------------- 902 (matrixBrightnessAutoAddress)
+Matrix Brightness
+---------------- 901 (matrixBrightnessAddress)
+LCD Brightness (byte)
+---------------- 900 (lcdBrightnessAddress)
+*/
